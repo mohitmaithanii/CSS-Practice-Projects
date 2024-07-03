f42e600646e60f6b4222e9a3670515268054f5ff
mohitmaithanii <mohitmaithani5@gmail.com>
2024-07-03 05:54:39 +0530

    Music web portfolio

---
+++ Portfolio Web/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				lightBlack: "#0F0F10",
+				lightGray: "#D9D9D9",
+				white: "#E7E7E4",
+				cream: "#DCD7CE",
+			},
+		},
+	},
+	plugins: [],
+};
