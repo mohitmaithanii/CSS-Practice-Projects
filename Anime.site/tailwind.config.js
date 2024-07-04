1cbddc597456da0902f12e4b85f4e06eeea066a4
mohitmaithanii <mohitmaithani5@gmail.com>
2024-07-04 16:36:00 +0530

    Anime. theme site

---
+++ Anime.site/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				skyBlue: "#a2d2ff",
+				lightSkyBlue: "#bde0fe",
+				pink: "#ffafcc",
+				lightPink: "#ffc8dd",
+				purple: "#c77dff",
+				lightPurple: "#e0aaff",
+				indigo: "#c8b6ff",
+			},
+		},
+	},
+	plugins: [],
+};
